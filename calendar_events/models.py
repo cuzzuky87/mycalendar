3c2d25ac78bf0fc828c7ad1c99f08c02c2e04610
cuzzuky87 <kazu_l_k@yahoo.co.jp>
2020-01-11 12:09:41 +0900

    added event model

---
+++ calendar_events/models.py
@@ -0,0 +1,19 @@
+import uuid
+from django.db import models
+
+from accounts.models import User
+
+class CalendarEvent(models.Model):
+    """Calendar modle """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_at = models.DateTimeField()
+    end_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
