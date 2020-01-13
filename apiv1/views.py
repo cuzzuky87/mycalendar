774f6073bbcad5e2ee0ada915612f77193cc6d30
cuzzuky87 <kazu_l_k@yahoo.co.jp>
2020-01-13 20:51:52 +0900

    WIP:permisson

--- apiv1/views.py
+++ apiv1/views.py
@@ -1,11 +1,16 @@
 from django.contrib.auth import get_user_model
 
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 
 from .serializers import CalendarEventReadSerializer
+from .permissions import IsOwnerOrReadOnly
 from calendar_events.models import CalendarEvent
 
 class CalendarEventViewset(viewsets.ModelViewSet):
     queryset = CalendarEvent.objects.all()
     serializer_class = CalendarEventReadSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
