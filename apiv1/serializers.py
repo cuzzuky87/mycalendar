5d83cf639d97f70065ce8ef3bc3429cc75797aba
cuzzuky87 <kazu_l_k@yahoo.co.jp>
2020-01-14 23:22:00 +0900

    mod json shape

--- apiv1/serializers.py
+++ apiv1/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
 
 from calendar_events.models import CalendarEvent
+from accounts.models import User
+
+class UserSerializerForCalendarModel(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id',)
+        extra_kwargs = {
+            'id': {
+                'read_only':True,
+            }
+        }
 
 class CalendarEventReadSerializer(serializers.ModelSerializer):
+    user = UserSerializerForCalendarModel()
     class Meta:
         model = CalendarEvent
         fields = '__all__'
