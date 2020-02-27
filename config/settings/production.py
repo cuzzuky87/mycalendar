45a4f57181482673dae59ec557d8294f5335a17b
cuzzuky87 <kazu_l_k@yahoo.co.jp>
2020-02-27 23:38:11 +0900

    デプロイ準備

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,9 +2,10 @@ from .base import *
 import dj_database_url
 import django_heroku
 
-django_heroku.settings(locals())
 
 DEBUG = False
 
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+django_heroku.settings(locals())
