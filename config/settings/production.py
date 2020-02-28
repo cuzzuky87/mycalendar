4fb2199548670903528902627714bef96358e5ae
cuzzuky87 <kazu_l_k@yahoo.co.jp>
2020-02-28 20:09:29 +0900

    whitenoiseの設定

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,4 +8,6 @@ DEBUG = False
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 django_heroku.settings(locals())
