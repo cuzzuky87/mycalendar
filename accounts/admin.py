735ceb91cb16ca79d19d54a774b141d8a0a74a45
cuzzuky87 <kazu_l_k@yahoo.co.jp>
2020-02-29 23:44:27 +0900

    微修正

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,36 +4,4 @@ from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.utils.translation import ugettext_lazy as _
 from .models import User
 
-#class MyUserChageForm(UserChangeForm):
-#    class Meta:
-#        model = User
-#        fields = '__all__'
-#
-#
-#class MyUserCreationForm(UserCreationForm):
-#    class Meta:
-#        model = User
-#        fields = ('email',)
-#
-#class MyUserAdmin(UserAdmin):
-#    fieldsets = (
-#        (None, {'fields': ('email', 'password')}),
-#        (_('Personal info'), {'fields': ('display_name',)}),
-#        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-#                                        'group', 'user_permissions')}),
-#        (_('Importtant dates'), {'fields': ('last_login', 'date_modified', 'date_joined')}),
-#    )
-#    add_fieldsets = (
-#        (None, {
-#            'classes': ('wide',),
-#            'fields': ('email', 'password1', 'password2'),
-#        }),
-#    )
-#    form = MyUserChageForm
-#    add_form = MyUserChageForm
-#    list_display = ('email', 'display_name', 'is_staff')
-#    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
-#    search_fields = ('email', 'display_name',)
-#    ordering = ('email',)
-
 admin.site.register(User)
