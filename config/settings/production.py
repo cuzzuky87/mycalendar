498d011c786221544d41999f7239eaa431d8a5e2
cuzzuky87 <kazu_l_k@yahoo.co.jp>
2020-03-01 14:40:57 +0900

    修正

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ import django_heroku
 DEBUG = False
 
 DATABASES = {}
-db_from_env = dj_database_url(conn_max_age=600)
+db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
