7de17c9555a4f0a69cdd1622afe8f2748b680d66
cuzzuky87 <kazu_l_k@yahoo.co.jp>
2020-03-01 15:06:57 +0900

    修正

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,7 +7,7 @@ DEBUG = False
 
 DATABASES = {}
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = db_from_env
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
